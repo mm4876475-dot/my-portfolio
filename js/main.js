009fd830008710bcb6152fb4486d462f35182a6e
mm4876475-dot <mm4876475@gmail.com>
2025-11-08 06:10:42 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//add class active to header on scrole
+
+let header = document.querySelector("header")
+
+window.onscroll= function(){
+    if(this.scrollY >=50){
+        header.classList.add("active");
+    }
+    else
+    {
+        header.classList.remove("active");
+    }
+}
